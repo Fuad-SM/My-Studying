fe406d503bbc99c19033bd0f7d68a476e25cb444
Fuad-SM <rzaki804@gmail.com>
2021-09-03 23:02:00 +0800

    Menghapus file sanber flutter

--- sanberappflutter/lib/tugas/tugas12/common/theme12.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryColor = Color(0xff507CA1);
-Color numberColor = Color(0xffADD1EF);
-Color fontColor = Color(0xff383D42);
-Color darkGrey = Color(0xffB4B4B4);
-Color greyColor = Color(0xffA5A5A6);
