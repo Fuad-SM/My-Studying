64fa89e21c446ec48d83492a3e401eec508db014
Fuad-SM <rzaki804@gmail.com>
2021-09-07 13:38:47 +0800

    menggunakan listview separated

--- BWA/bwa_cozy/lib/widgets/photo_card.dart
+++ BWA/bwa_cozy/lib/widgets/photo_card.dart
@@ -1,5 +1,7 @@
 import 'package:bwa_cozy/models/space.dart';
+import 'package:bwa_cozy/providers/space_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PhotoCard extends StatelessWidget {
   const PhotoCard(this.space);
@@ -7,24 +9,40 @@ class PhotoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var spaceProvider = Provider.of<SpaceProvider>(context);
     return Container(
       height: 88,
-      child: ListView(
+      width: MediaQuery.of(context).size.width,
+      child: ListView.separated(
           scrollDirection: Axis.horizontal,
-          children: space.photos.map((item) {
-            return Container(
-              margin: EdgeInsets.only(right: 18),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.network(
-                  item,
+          itemBuilder: (BuildContext context, int index) {
+            return FutureBuilder(
+              future: spaceProvider.getRecomendedSpaces(),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData) {
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.network(
+                      space.photos[index],
+                      width: 110,
+                      height: 88,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }
+                return Container(
                   width: 110,
-                  height: 88,
-                  fit: BoxFit.cover,
-                ),
-              ),
+                  height: 80,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
             );
-          }).toList()),
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              SizedBox(width: 18),
+          itemCount: space.photos.length),
     );
   }
 }
